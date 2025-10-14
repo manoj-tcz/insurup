06b0d42f50e261ef4d611a02909ebb261056646e
manoj yadav <manoj.yadav@techchefz.com>
2025-10-14 15:14:39 +0530

    added colors.js file

---
+++ styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primaryColor: "#125C80",   
+  secondaryColor: "#479685",      
+  backgroundColor: "#FFFFFF", 
+  textColor: "#111111",  
+  buttoncolor: "#125C80",
+  cardColor: "#ED9923"     
+};
+
+export default colors;
